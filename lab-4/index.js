6e394ed96a3b98c916b21177849ff4993f6eb37b
Richard Mac <rmac2@my.bcit.ca>
2022-10-02 21:06:18 -0700

    added challenges

--- lab-4/index.js
+++ lab-4/index.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose")
 const app = express()
 const port = 5000
 
-app.listen(process.env.PORT| port, async()=>{
+app.listen(process.env.PORT || port, async()=>{
     try{
         await mongoose.connect("mongodb+srv://xr1chard:1234@cluster0.0zutyxg.mongodb.net/test")
     }catch(error){
@@ -46,15 +46,7 @@ app.get('/api/v2/unicorns', (req, res) => {
 
 
   app.get('/api/v2/unicorn/:id', (req, res) => {
-    // var found = false
-    // for (i = 0; i < unicornsJSON.length; i++) {
-    //   if (unicornsJSON[i]._id == req.params.id) {
-    //     found = true
-    //     break
-    //   }
-    // }
-    // if (found) { res.json(unicornsJSON[i]); return }
-    // res.json({ msg: "not found" })
+
     console.log(req.params.id);
     unicornModel.find({ _id: mongoose.Types.ObjectId(`${req.params.id}`) })
       .then(doc => {
@@ -69,44 +61,18 @@ app.get('/api/v2/unicorns', (req, res) => {
 
 app.use(express.json())
 app.post('/api/v2/unicorn', (req, res) => {
-  // - create a new unicorn
-
-  // unicornsJSON.push(req.body)
-  //update the file
-  // writeFileAsync('./data.js', JSON.stringify(unicornsJSON), 'utf-8')
-  //   .then(() => { })
-  //   .catch((err) => { console.log(err); })
 
   unicornModel.create(req.body, function (err) {
     if (err) console.log(err);
-    // saved!
   });
   res.json(req.body)
 })  
 
 app.patch('/api/v2/unicorn/:id', (req, res) => {
-    // - update a unicorn
-  
-    // unicornsJSON = unicornsJSON.map(({ _id, ...aUnicorn }) => {
-    //   // console.log(req.body);
-    //   if (_id == req.body._id) {
-    //     console.log("Bingo!");
-    //     return req.body
-    //   } else
-    //     return aUnicorn
-    // })
-    // console.log(unicornsJSON);
-  
-  
-    //update the file
-    // writeFileAsync('./data.js', JSON.stringify(unicornsJSON), 'utf-8')
-    //   .then(() => { })
-    //   .catch((err) => { console.log(err); })
-    // console.log(req.body);
+
     const { _id, ...rest } = req.body;
     unicornModel.updateOne({ _id: mongoose.Types.ObjectId(req.params.id) }, rest, function (err, res) {
-      // Updated at most one doc, `res.nModified` contains the number
-      // of docs that MongoDB updated
+
       if (err) console.log(err)
       console.log(res)
     });
@@ -115,17 +81,48 @@ app.patch('/api/v2/unicorn/:id', (req, res) => {
   })
 
   app.delete('/api/v2/unicorn/:id', (req, res) => {
-    // - delete a unicorn
-    // unicornsJSON = unicornsJSON.filter((element) => element._id != req.params.id)
-  
-    // //update the file
-    // writeFileAsync('./data.js', JSON.stringify(unicornsJSON), 'utf-8')
-    //   .then(() => { })
-    //   .catch((err) => { console.log(err); })
+
     unicornModel.deleteOne({ _id: mongoose.Types.ObjectId(req.params.id) }, function (err, result) {
       if (err) console.log(err);
       console.log(result);
     });
   
     res.send("Deleted successfully?")
+  })
+
+
+  app.patch('/api/v2/unicornNewLovesFood/:id/', (req, res) => {
+    unicornModel.updateOne({ _id: mongoose.Types.ObjectId(req.params.id) }, req.body, function (err, res) {
+
+      if (err) console.log(err)
+      console.log(res)
+    });
+  
+    res.send("Updated successfully!")
+  })
+  
+  app.patch('/api/v2/unicornAddLovesFood/:id/', (req, res) => {
+    unicornModel.updateOne({ _id: mongoose.Types.ObjectId(req.params.id) }, {
+      $push: {
+        loves: req.body.newLoves
+      }
+    }, function (err, res) {
+  
+      if (err) console.log(err)
+      console.log(res)
+    });
+    res.send("Updated successfully!")
+  })
+  
+  app.patch('/api/v2/unicornRemoveLovesFood/:id/:item', (req, res) => {
+    unicornModel.updateOne({ _id: mongoose.Types.ObjectId(req.params.id) }, {
+      $pull: {
+        loves: req.params.item
+      }
+    }, function (err, res) {
+ 
+      if (err) console.log(err)
+      console.log(res)
+    });
+    res.send("Updated successfully!")
   })
