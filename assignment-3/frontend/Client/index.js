3beaad0af418fcc0570cdbc9fc480636841cb61b
Richard Mac <78776822+richardmac604@users.noreply.github.com>
2022-12-03 21:04:43 -0800

    added search and pagination to index

--- assignment-3/frontend/Client/index.js
+++ assignment-3/frontend/Client/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import React, { useState, useRef, useEffect } from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import FilteredPagination from './FilteredPagination'
+import Search from './Search'
+import axios from 'axios'
 import Login from './Login'
 import {
   BrowserRouter as Router
@@ -14,5 +20,9 @@ root.render(
         <Login />
       </Router>
     </h1>
+    <div>
+    <Search types={types} checkedState={checkedState} setCheckedState={setCheckedState} />
+    <FilteredPagination types={types} checkedState={checkedState} />
+    </div>
   </React.StrictMode>
 );
